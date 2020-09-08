22562077d89823342009f9c424df05ae593d089b
yonaminegit <y.yonamine@saltyster.com>
2020-09-08 17:55:00 +0900

    #32 オブジェクトのメンバを実行する

--- object.js
+++ object.js
@@ -1,10 +1,14 @@
 var person = {
   'name' : 'bob',
   'age' : '32',
+
+  selfIntroduce : function () {
+      for (var value of Object.values(person)) {
+       console.log(value);    
+      }
+  },
 };
 
 person['country'] = 'canada';
 
-for (var value of Object.values(person)) {
-  console.log(value);    
-}
+person.selfIntroduce();
