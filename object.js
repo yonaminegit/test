1b396cf5c8855ebf927a5a9f593422666cd2884e
yonaminegit <y.yonamine@saltyster.com>
2020-09-09 09:49:38 +0900

    #32 オブジェクトの実装の練習　 コミットを間違えたため再送します。

--- object.js
+++ object.js
@@ -8,6 +8,12 @@ var person = {
   'name' : 'bob',
   'age' : '32',
   
+  selfIntroduce : function () {
+    for (var value of Object.values(person)) {
+     console.log(value);    
+    }
+  },
+
   family : {
     father: 'Bobby',
     mother: 'Cathy',
@@ -17,6 +23,30 @@ var person = {
 
 person['country'] = 'canada';
 
+console.log('1.');
+console.log(person.name);
+
+console.log('2.');
+console.log(person.country);
+
+console.log('3.');
+for (var value of Object.values(person)) {
+  console.log(value);    
+ }
+
+ console.log('4.');
+person.selfIntroduce();
+
+console.log('5.');
+for (var value of Object.values(person)) {
+  console.log(value);    
+ }
+
+console.log('7.');
+console.log(person.family.father);
+console.log(person.family.mother);
+
+console.log('8.');
 for(var value of array){
   console.log(value);
 }
